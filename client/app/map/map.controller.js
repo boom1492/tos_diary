bcb129671e87f996afbf8f82d7e0188355a11202
Junghyun Lee <soul1492@gmail.com>
2016-02-16 16:41:42 +0900

    remove test log

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -7,7 +7,7 @@ class MapController {
   constructor($http, $scope, socket) {
     this.$http = $http;
     this.maps = [];
-
+/*
     $http.get('/api/maps').then(response => {
       this.maps = response.data;
       socket.syncUpdates('map', this.maps);
@@ -15,7 +15,7 @@ class MapController {
 
     $scope.$on('$destroy', function() {
       socket.unsyncUpdates('map');
-    });
+    });*/
   }
 
 }
