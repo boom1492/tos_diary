70d2fcc46fd06867e9fc8d4cf27d669b77c1ee41
Junghyun Lee <soul1492@gmail.com>
2016-03-04 12:19:55 +0900

    add worldmap

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -6,17 +6,36 @@ class MapController {
 
   constructor($http, $scope, socket) {
     this.$http = $http;
-    this.maps = [];
-/*
+    $scope.block_maps = [];
+    $scope.group_maps = [];
+    $scope.group_names = [];
+
     $http.get('/api/maps').then(response => {
-      this.maps = response.data;
-      socket.syncUpdates('map', this.maps);
+      //$scope.block_maps = response.data;
+                   
+      /*
+      $scope.block_maps = response.data.filter(function(item){
+        return item.group == undefined;
+      }); 
+      */
+      $scope.group_maps = response.data.filter(function(item){
+        return item.group != undefined;
+      });
+      
+      
+      var group = _.groupBy(response.data, function(item){
+        return item.group;
+      });
+      
+      $scope.block_maps = group["undefined"];
+      
+      //$scope.group_maps = group.delete("undefined");
+      delete group["undefined"];
+      
+      $scope.group_names = _.map(group, function(item){ return {group: item[0].group, location: item[0].location}});
+      
     });
 
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('map');
-    });*/
-    
   }
 
 }
