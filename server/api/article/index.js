d62ee9c5ddd585e5204c0a8c2eb02a503316f006
Junghyun Lee <soul1492@gmail.com>
2016-03-10 01:48:26 +0900

    fix font

--- server/api/article/index.js
+++ server/api/article/index.js
@@ -9,9 +9,9 @@ var router = express.Router();
 router.get('/summary', controller.indexOnlyTitle);
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.post('/', auth.isAuthenticated(), controller.create);
-router.put('/:id', auth.isAuthenticated(), controller.update);
-router.patch('/:id', auth.isAuthenticated(), controller.update);
-router.delete('/:id', auth.isAuthenticated(), controller.destroy);
+router.post('/', auth.isAuthenticated(), auth.hasRole('admin'), controller.create);
+router.put('/:id', auth.isAuthenticated(), auth.hasRole('admin'), controller.update);
+router.patch('/:id', auth.isAuthenticated(), auth.hasRole('admin'), controller.update);
+router.delete('/:id', auth.isAuthenticated(), auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
